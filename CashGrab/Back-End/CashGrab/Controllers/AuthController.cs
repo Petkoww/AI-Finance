f48e3118168ebdb5140708cab4c296ba9003260d
Petkoww <116186028+Petkoww@users.noreply.github.com>
2023-10-12 21:09:35 +0300

    Upload 2

---
+++ CashGrab/Back-End/CashGrab/Controllers/AuthController.cs
@@ -0,0 +1,112 @@
+﻿using AIF.Dtos;
+using AIF.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using AIF.Models.GoogleModels;
+
+namespace AIF.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            try
+            {
+                return await _authService.RegisterAsync(dto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            try
+            {
+                return await _authService.LoginAsync(dto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> GoogleLogin(GoogleLoginDto dto)
+        {
+            try
+            {
+                return await _authService.GoogleLoginAsync(dto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPost("Validate-Google-Token")]
+        public async Task<GoogleTokenValidationResult> ValidateGoogleToken(string googleToken)
+        {
+            try
+            {
+                return await _authService.ValidateGoogleTokenAsync(googleToken);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpGet("user")]
+        public async Task<IActionResult> GetUser()
+        {
+            try
+            {
+                return await _authService.GetUserAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPost("update-user-info")]
+        public async Task<IActionResult> UpdateUserInfo(UpdateUserInfoDto dto)
+        {
+            try
+            {
+                return await _authService.UpdateUserInfoAsync(dto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                return await _authService.LogoutAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CashGrab/Back-End/CashGrab/Data/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using AIF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace AIF.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AifDatabaseContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserRepository(AifDatabaseContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> GetLoggedInUser()
+        {
+            var userName = _httpContextAccessor.HttpContext.User.Identity.Name;
+            return await _context.Users.FirstOrDefaultAsync(u => u.Name == userName);
+        }
+
+        public async Task<User> UpdateProfilePictureUrlAsync(int userId, string profilePictureUrl)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user != null)
+            {
+                user.ProfilePictureUrl = profilePictureUrl;
+                await _context.SaveChangesAsync();
+            }
+            return user;
+        }
+    }
+}
